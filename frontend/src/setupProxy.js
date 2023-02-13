e16e3f86a307f2612d00194e71f772ccfe852995
Yier Dong <65011819+Dong34@users.noreply.github.com>
2023-02-13 18:02:58 -0500

    change localhost to 127.0.0.1

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
     '/data',
     createProxyMiddleware({
-        target: 'http://localhost:5000',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
     })
     );
