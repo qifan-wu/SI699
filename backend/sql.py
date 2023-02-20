28a11a81d561ee8777ca2498e46be7971e0752e3
Yier Dong <65011819+Dong34@users.noreply.github.com>
2023-02-20 18:58:15 -0500

    Update sql.py

--- backend/sql.py
+++ backend/sql.py
@@ -1,9 +1,15 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="si699",
-  password="password"
-)
+def main():
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user="si699",
+    password="password"
+    database="mydatabase"
+  )
+  mycursor = mydb.cursor()
+  mycursor.execute("CREATE TABLE si699_table (id INT NOT NULL PRIMARY KEY, url VARCHAR(255), date DATETIME)")
 
-print(mydb)
+
+if __name__=="__main__":
+  main()
