4ef2092a39f0f4ec99f14f7ea7fb0c857bcd4ea1
Yier Dong <65011819+Dong34@users.noreply.github.com>
2023-02-20 19:00:15 -0500

    Add files via upload

---
+++ backend/predict.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import seaborn as sb
+import matplotlib.pyplot as plt
+from nltk.tokenize import RegexpTokenizer
+import nltk
+#nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer  
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB 
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix,mean_squared_error,precision_score,recall_score,f1_score
+from sklearn.metrics import classification_report , roc_curve, f1_score, accuracy_score, recall_score , roc_auc_score,make_scorer
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+
+import pickle
+from sklearn.svm import LinearSVC
+
+def get_accuracy(name, trained_model , x_train, y_train, x_test, y_test):
+    tree_predict = trained_model.predict(x_test)
+    print("Testing accuracy   :",metrics.accuracy_score(y_test, tree_predict)*100 , "%")
+    print("MSE [TEST]          :",mean_squared_error(y_test, tree_predict))
+
+
+    tree_predict1 = trained_model.predict(x_train)
+    print("Training accuracy  :",metrics.accuracy_score(y_train, tree_predict1)*100 ,"%")
+    print("MSE [TRAIN]         :",mean_squared_error(y_train, tree_predict1))
+
+    print("precision : ",precision_score(y_test, tree_predict,average='micro'))
+    print("recall    : ",recall_score(y_test, tree_predict,average='micro'))
+    print("f1_score  : ",f1_score(y_test, tree_predict,average='micro'))
+
+
+    cf1 = confusion_matrix(y_test,tree_predict)
+    sb.heatmap(cf1,annot=True,fmt = '.0f')
+    plt.xlabel('prediction')
+    plt.ylabel('Actual')
+    plt.title(name + ' Confusion Matrix')
+    plt.show()
+
+    print(classification_report(y_train,  trained_model.predict(x_train)))
+    print(classification_report(y_test,  trained_model.predict(x_test)))
+
+def train_save_model(df):
+    df_train, df_test = train_test_split(df, random_state=42,test_size=0.2,shuffle=True)
+    cv = CountVectorizer()
+
+    x_train = cv.fit_transform(df_train.lem_url.astype('str')) 
+    x_test = cv.transform(df_test.lem_url.astype('str'))
+    
+    y_train = df_train.Label
+    y_test = df_test.Label
+    y_train=np.where(y_train=='bad',1,0)
+    y_test=np.where(y_test=='bad',1,0)
+
+    model = LinearSVC()
+    model.fit(x_train, y_train)
+
+    # Save the trained model to a file using pickle
+    with open('model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+
+    return get_accuracy('LinearSVC', model , x_train, y_train, x_test, y_test)
+
+
+def predict_label(input_data):
+    # Load the saved model
+    with open('model.pkl', 'rb') as f:
+        model = pickle.load(f)
+    
+    # Make the prediction
+    label = model.predict(input_data)
+    return label
